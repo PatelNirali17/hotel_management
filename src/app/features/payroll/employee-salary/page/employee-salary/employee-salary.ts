3f3dbe602147f5ef073e8e85d6a86559733be010
patelNirali17 <niralibpatel13@gmail.com>
2026-01-03 18:49:36 +0530

    Employee Salary page set

--- src/app/features/payroll/employee-salary/page/employee-salary/employee-salary.ts
+++ src/app/features/payroll/employee-salary/page/employee-salary/employee-salary.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../../../shared/shared-module';
 
 @Component({
   selector: 'app-employee-salary',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './employee-salary.html',
   styleUrl: './employee-salary.scss',
 })
 export class EmployeeSalary {
+  
 
 }
